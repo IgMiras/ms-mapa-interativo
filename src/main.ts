39c2825784a84207b57dd02012336d01f774a2a8
IgMiras <im.miras@unesp.br>
2024-10-14 10:11:00 -0300

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app/app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     snapshot: true,
+    cors: true,
   });
   await app.listen(3000);
 }
